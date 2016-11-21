82ac6b98f8417d8a88e78c0a5a8bfb541a1cb0b4
Maicon <maicon@dynamix.com.br>
2016-11-21 13:53:46 -0200

    ajustes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -9,11 +8,32 @@ import java.util.Scanner;
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
+		StringBuilder str = new StringBuilder();
+		str.append("12\n");
+		str.append("0 1 10\n");
+		str.append("0 5 5\n");
+		str.append("1 5 4\n");
+		str.append("1 2 2\n");
+		str.append("2 7 15\n");
+		str.append("2 3 7\n");
+		str.append("3 4 1\n");
+		str.append("3 8 20\n");
+		str.append("4 11 30\n");
+		str.append("5 6 1\n");
+		str.append("6 7 2\n");
+		str.append("6 9 6\n");
+		str.append("7 8 1\n");
+		str.append("8 10 5\n");
+		str.append("9 10 4\n");
+		str.append("10 11 5\n");
+		str.append("0 0 0\n");
+		
+		
 		System.out.println("-- Programa para eulerizar um grafo e imprimir o ciclo euleriano --");
 		System.out.println("Para este programa foi utilizado o grafo exemplo do trabalho... (conforme grafo.png nesse projeto)");
 		System.out.println("");
 
-		Scanner scanner = new Scanner(new File("arestas.txt"));
+		Scanner scanner = new Scanner(str.toString());
 
 		int qtdVertices = Integer.parseInt(scanner.nextLine());
 
